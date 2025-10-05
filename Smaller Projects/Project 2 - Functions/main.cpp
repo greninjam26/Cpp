2ae9514ee4c5b94cb9ceac76668d6f325b41b733
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-10-05 17:15:58 -0400

    implement first function

--- Smaller Projects/Project 2 - Functions/main.cpp
+++ Smaller Projects/Project 2 - Functions/main.cpp
@@ -2,8 +2,39 @@
 #include <cassert>
 
 int main();
+void pattern(unsigned int n);
+
+// this function take in n and output a pattern of * base on the n value
+void pattern(unsigned int n) {
+    for (int r = 0; r <= n; r++) {
+        for (int c = 0; c < n*2+1; c++) {
+            if (c < r || c+r >= n*2+1) {
+                std::cout << " ";
+            }
+            else {
+                std::cout << "*";
+            }
+        }
+        std::cout << std::endl;
+    }
+    for (int r = n-1; r >= 0; r--) {
+        for (int c = 0; c < n*2+1; c++) {
+            if (c < r || c+r >= n*2+1) {
+                std::cout << " ";
+            }
+            else {
+                std::cout << "*";
+            }
+        }
+        std::cout << std::endl;
+    }
+}
 
 int main () {
+    // first function
+    int n{0};
+    std::cin >> n;
+    pattern(n);
     
     return 0;
 }
