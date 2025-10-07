4e04236d125a5d8620e7f4ce6459820d1ffef953
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-10-07 17:24:16 -0400

    check input and bug fix

--- Smaller Projects/Project 2 - Functions/main.cpp
+++ Smaller Projects/Project 2 - Functions/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cassert>
+#include <climits>
 
 int main();
 void pattern(unsigned int n);
@@ -35,6 +36,7 @@ void pattern(unsigned int n) {
 
 // this function take the input n and calculate log10(n)
 unsigned int log10(unsigned int n) {
+    assert(n != 0);
     unsigned int count{0};
     while (n >= 10) {
         n /= 10;
@@ -45,6 +47,10 @@ unsigned int log10(unsigned int n) {
 
 // this function take in a number and a bit(0, 1), then check the number of the bit is showed up in the binary version of the number
 unsigned int count(unsigned int n, unsigned int bit) {
+    assert(bit == 1 || bit == 0);
+    if (n == bit) {
+        return 1;
+    }
     unsigned int counter{0};
     while (n > 0) {
         if (n%2 == bit) {
@@ -65,55 +71,60 @@ unsigned int count(unsigned int n, unsigned int bit) {
 // 3, 4. 1243
 // 4289396650
 unsigned int swap_bytes(unsigned int n, unsigned int b0, unsigned int b1) {
-    if (b0 != b1) {
-        unsigned int b0byte = ((n & (255 << b0 * 8)) >> (b0 * 8)) << (b1 * 8);
-        unsigned int b1byte = ((n & (255 << b1 * 8)) >> (b1 * 8)) << (b0 * 8);
-        std::cout << b0byte << std::endl;
-        std::cout << b1byte << std::endl;
-        unsigned int clearbytes{0};
-        for (int i = 0; i < 4; i++) {
-            clearbytes <<= 8;
-            std::cout << "?" << clearbytes << std::endl;
-            if (i != b0 && i != b1) {
-                std::cout << i << " " << b0 << " " << b1 << std::endl;
-                clearbytes |= 255;
-            }
-            std::cout << "??" << clearbytes << std::endl;
-        }
-        std::cout << "clear: " << clearbytes << std::endl;
-        n &= clearbytes;
-        std::cout << "n: " << n << std::endl;
-        n |= b0byte;
-        n |= b1byte;
+    assert((b0 >= 0 && b0 <= 3) && (b1 >= 0 && b1 <= 3));
+    // std::cout << "i: " << n << std::endl;
+    if (b0 == b1) {
+        return n;
     }
-    return n;
+    unsigned int swapped{n};
+    unsigned int b0byte = ((n & (255 << b0 * 8)) >> (b0 * 8)) << (b1 * 8);
+    unsigned int b1byte = ((n & (255 << b1 * 8)) >> (b1 * 8)) << (b0 * 8);
+    // std::cout << b0byte << std::endl;
+    // std::cout << b1byte << std::endl;
+    unsigned int clearbytes{0};
+    for (int i = 3; i >= 0; i--) {
+        clearbytes <<= 8;
+        // std::cout << "?" << clearbytes << std::endl;
+        if (i != b0 && i != b1) {
+            // std::cout << i << " " << b0 << " " << b1 << std::endl;
+            clearbytes |= 255;
+        }
+        // std::cout << "??" << clearbytes << std::endl;
+    }
+    // std::cout << "clear: " << clearbytes << std::endl;
+    // std::cout << "swapped: " << swapped << std::endl;
+    swapped &= clearbytes;
+    // std::cout << "swappedc: " << swapped << std::endl;
+    swapped |= b0byte;
+    swapped |= b1byte;
+    return swapped;
 }
 
 int main () {
-    // // first function
-    // unsigned int patterns_input{0};
-    // std::cout << "Please enter a number to generate a pattern: " << std::endl;
-    // std::cin >> patterns_input;
-    // pattern(patterns_input);
+    // first function
+    unsigned int patterns_input{0};
+    std::cout << "Please enter a number to generate a pattern: " << std::endl;
+    std::cin >> patterns_input;
+    pattern(patterns_input);
 
-    // // second function
-    // unsigned int logn{0};
-    // unsigned int result{0};
-    // std::cout << "Please enter a number to be calculated by log: " << std::endl;
-    // std::cin >> logn;
-    // result = log10(logn);
-    // std::cout << "result: " << result << std::endl;
+    // second function
+    unsigned int logn{0};
+    unsigned int result{0};
+    std::cout << "Please enter a number to be calculated by log: " << std::endl;
+    std::cin >> logn;
+    result = log10(logn);
+    std::cout << "result: " << result << std::endl;
 
-    // // third function
-    // unsigned int initial_number{0};
-    // unsigned int bit_check{0};
-    // unsigned int bit_count{0};
-    // std::cout << "Please enter the number to be checked: ";
-    // std::cin >> initial_number;
-    // std::cout << "Please enter the bit you want to check: ";
-    // std::cin >> bit_check;
-    // bit_count = count(initial_number, bit_check);
-    // std::cout << bit_count << std::endl;
+    // third function
+    unsigned int initial_number{0};
+    unsigned int bit_check{0};
+    unsigned int bit_count{0};
+    std::cout << "Please enter the number to be checked: ";
+    std::cin >> initial_number;
+    std::cout << "Please enter the bit you want to check: ";
+    std::cin >> bit_check;
+    bit_count = count(initial_number, bit_check);
+    std::cout << bit_count << std::endl;
 
     // fourth function
     unsigned int initial_bytes{0};
@@ -128,7 +139,6 @@ int main () {
     unsigned int swapped_bytes{0};
     swapped_bytes = swap_bytes(initial_bytes, b0, b1);
     std::cout << "result: " << swapped_bytes << std::endl;
-    // std::cout << ((swapped_bytes << 8) | 255) << std:: endl;
     
     return 0;
 }
