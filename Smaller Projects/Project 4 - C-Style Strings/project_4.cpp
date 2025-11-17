bc27a1334b6e3685adaf8ce92a736ef61a0ab1e7
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-11-17 18:42:41 -0500

    define all the functions

--- Smaller Projects/Project 4 - C-Style Strings/project_4.cpp
+++ Smaller Projects/Project 4 - C-Style Strings/project_4.cpp
@@ -1,1 +1,26 @@
 #include "p_4_header.hpp"
+#include <iostream>
+
+std::size_t length( char const *a ){
+    return 0;
+}
+int compare( char const *str1, char const *str2 ){
+    return 0;
+}
+void assign( char *str1, char const *str2 ){}
+unsigned int distance( char const *str1, char const *str2 ){
+    return 0;
+}
+std::size_t is_sorted( char *array[], std::size_t capacity ){
+    return 0;
+}
+void insert( char *array[], std::size_t capacity ){}
+void insertion_sort( char *array[], std::size_t capacity ){}
+std::size_t remove_duplicates( char *array[], std::size_t capacity ){
+    return 0;
+}
+std::size_t find( char *array[], std::size_t capacity, char const *str ){
+    return 0;
+}
+
+void free_word_array( char** word_array ){}
